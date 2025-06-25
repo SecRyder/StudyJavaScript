b7c2690963691ec85506895b49726b1340929a1a
Trần Thị Ánh Nguyệt <167234889+SecRyder@users.noreply.github.com>
2025-06-25 17:40:13 +0700

    Add files via upload

---
+++ KhaiBaoBienVaKhoiTaoBienMain.js
@@ -0,0 +1,26 @@
+// Ham hien thi hop thoai len tren trinh duyet
+alert("Hello! I'm AnMunSR");
+
+// Ham hien thi hop thoai nhap lieu cho nguoi dung
+var x = prompt("Moi nhap ten cua ban: ");
+
+/*
+var: Tu khoa khai bao bien truoc ES6
+let: Tu khoa khai bao bien duoc gioi thieu tu ES6
+const: Khai bao hang so
+ */
+
+// Khai bao bien
+var myName;
+let yourName;
+const pi = 3.14;
+
+// Khoi tao bien
+var diemToan = 10;
+let firstName = "AnMunSR";
+
+// Xuat gia tri cua bien trong console 
+console.log(firstName);
+console.log(diemToan);
+console.log(x);
+
