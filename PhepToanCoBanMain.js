eb80d303f2fe834c3d3794bff53dc8418a7f5b98
Trần Thị Ánh Nguyệt <167234889+SecRyder@users.noreply.github.com>
2025-06-25 18:10:58 +0700

    Add files via upload

---
+++ PhepToanCoBanMain.js
@@ -0,0 +1,18 @@
+alert("Hello")
+let a = 5;
+let b = 2;
+
+// Phep toan co ban
+let tong = a + b;
+let hieu = a - b;
+let tich = a * b;
+let thuong = a / b;
+let chiaLayDu = a % b;
+
+
+// Xuat ket qua
+console.log("Tong =", tong);
+console.log("Hieu =", hieu);
+console.log("Tich =", tich);
+console.log("Thuong =", thuong);
+console.log("Chia lay du =", chiaLayDu);
