e34c081023f6657307195dd2e72f43aa39f70a46
Trần Thị Ánh Nguyệt <167234889+SecRyder@users.noreply.github.com>
2025-06-25 18:00:59 +0700

    Add files via upload

---
+++ KieuDuLieuMain.js
@@ -0,0 +1,29 @@
+alert("Chao nha");
+
+// Kiey string
+let myString = 'abc';
+console.log("My string: " + myString);
+console.log(typeof (myString));
+
+// Kieu number
+let myInt = 10;
+console.log("My int: " + myInt);
+console.log(typeof (myInt));
+
+// Kieu boolen
+let check = true;
+console.log(typeof (check));
+
+// Kieu undefined
+let under;
+console.log(typeof (under));
+
+// So nguyen an toan toi thieu/ tpi da
+console.log("So nguyen an toan toi da: " + Number.MAX_SAFE_INTEGER);
+console.log("So nguyen an toan toi thieu: " + Number.MIN_SAFE_INTEGER);
+
+
+// Cac cach xuat du lieu
+console.log("So a " + myInt);
+console.log("So a", myInt);
+console.log("So a %s", myInt);
