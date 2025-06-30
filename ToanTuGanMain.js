3f07b6b9b9cab3db9db177a22d4ecaabeb8062c2
Trần Thị Ánh Nguyệt <167234889+SecRyder@users.noreply.github.com>
2025-07-01 00:39:16 +0700

    Add files via upload

---
+++ ToanTuGanMain.js
@@ -0,0 +1,29 @@
+alert('Hello!')
+let x = 10;
+let y = 5;
+let z = x + y;
+console.log(x);
+console.log(y);
+console.log(z);
+
+// Gan += , -= , *= , /= , %=
+console.log('Ket qua phep gan: ');
+let m = 10;
+m += 5;
+console.log(m);
+
+let n = 8;
+n -= 3;
+console.log(n);
+
+let p = 6;
+p *= 2;
+console.log(p)
+
+let q = 9;
+q /= 3;
+console.log(q);
+
+let r = 4;
+r %= 3;
+console.log(r);
